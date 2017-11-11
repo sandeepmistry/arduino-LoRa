858d85454d0a6c1a22b84d5d2a77e9e8e02253cc
YannikW <YannikW@users.noreply.github.com>
2017-11-11 14:42:22 +0100

    fix SNR register

--- src/LoRa.cpp
+++ src/LoRa.cpp
@@ -17,8 +17,8 @@
 #define REG_FIFO_RX_CURRENT_ADDR 0x10
 #define REG_IRQ_FLAGS            0x12
 #define REG_RX_NB_BYTES          0x13
+#define REG_PKT_SNR_VALUE        0x19
 #define REG_PKT_RSSI_VALUE       0x1a
-#define REG_PKT_SNR_VALUE        0x1b
 #define REG_MODEM_CONFIG_1       0x1d
 #define REG_MODEM_CONFIG_2       0x1e
 #define REG_PREAMBLE_MSB         0x20
