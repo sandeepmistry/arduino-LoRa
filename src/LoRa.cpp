96ed2f52e312c9f2c7898c73ea58b45dcf7bee45
Sandeep Mistry <sandeep.mistry@gmail.com>
2017-12-21 20:18:45 -0500

    Stabilize onReceive(...) callback handling
    By using SPI.usingInterrupt(…) and SPI.notUsingInterrupt(…)

--- src/LoRa.cpp
+++ src/LoRa.cpp
@@ -281,9 +281,13 @@ void LoRaClass::onReceive(void(*callback)(int))
   if (callback) {
     writeRegister(REG_DIO_MAPPING_1, 0x00);
 
+    SPI.usingInterrupt(digitalPinToInterrupt(_dio0));
     attachInterrupt(digitalPinToInterrupt(_dio0), LoRaClass::onDio0Rise, RISING);
   } else {
     detachInterrupt(digitalPinToInterrupt(_dio0));
+#ifdef SPI_HAS_NOTUSINGINTERRUPT
+    SPI.notUsingInterrupt(digitalPinToInterrupt(_dio0));
+#endif
   }
 }
 
